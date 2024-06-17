1ae45fcef878ac5309e624accc2253a26e645dff
Paulo Pinheiro <paulo.pinheiro@dentsu.com>
2024-06-17 17:50:37 -0300

    feat(maps): section completed

--- exercises/maps/maps2/main.go
+++ exercises/maps/maps2/main.go
@@ -1,12 +1,14 @@
 // maps2
 // Make me compile!
-//
-// I AM NOT DONE
+
 package main
 
 import "fmt"
 
 func main() {
-	m := map{}
+	m := map[string]int{
+		"John": 30,
+		"Ana":  28,
+	}
 	fmt.Printf("John is %d and Ana is %d", m["John"], m["Ana"])
 }
