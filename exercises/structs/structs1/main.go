a6e900d27e73c004fd6505565ade5cd7a305ec16
Paulo Pinheiro <paulo.pinheiro@dentsu.com>
2024-06-17 18:11:37 -0300

    feat(structs): section completed

--- exercises/structs/structs1/main.go
+++ exercises/structs/structs1/main.go
@@ -1,15 +1,20 @@
 // structs1
 // Make me compile!
-//
-// I AM NOT DONE
+
 package main
 
 import "fmt"
 
 type Person struct {
-
+	name string
+	age  int
 }
 
 func main() {
+	person := Person{
+		name: "Paulo",
+		age:  31,
+	}
+
 	fmt.Printf("Person %s and age %d", person.name, person.age)
 }
--- exercises/structs/structs2/main.go
+++ exercises/structs/structs2/main.go
@@ -1,19 +1,25 @@
 // structs2
 // Make me compile!
-//
-// I AM NOT DONE
+
 package main
 
 import "fmt"
 
 type Person struct {
 	// don't just create the phone field here. embed a new struct
+	ContactDetails
 	name string
-	age int
+	age  int
+}
+
+type ContactDetails struct {
+	phone string
 }
 
 func main() {
-	// contactDetails := ContactDetails{}
-	person := Person{name: "John", age: 32}
+	contactDetails := ContactDetails{
+		phone: "123",
+	}
+	person := Person{name: "John", age: 32, ContactDetails: contactDetails}
 	fmt.Printf("%s is %d years old and his phone is %s\n", person.name, person.age, person.phone)
 }
